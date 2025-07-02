a340ec074dd34c8c2315260d9950203abc2250d5
Tazir <tazirxxx@gmail.com>
2025-07-02 14:43:10 +0300

    Implement add button and fix exit bug

--- graphical_user_interface.py
+++ graphical_user_interface.py
@@ -3,9 +3,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do:")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key='to-do')
 addb = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, addb]])
-window.read()
+window = sg.Window('My To-Do App', 
+                   layout=[[label], [input_box, addb]], 
+                   font=('Helvetica', 13))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.gtodos()
+            ntodo = values['to-do'] + '\n'
+            todos.append(ntodo)
+            functions.wtodos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
