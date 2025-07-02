e66fcedf588dda6eb1eaf5d1a0a716b71315a7c7
Tazir <tazirxxx@gmail.com>
2025-07-02 15:08:59 +0300

    Implement edit button

--- graphical_user_interface.py
+++ graphical_user_interface.py
@@ -3,23 +3,43 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do:")
-input_box = sg.InputText(tooltip="Enter to-do", key='to-do')
+input_box = sg.InputText(tooltip="Enter to-do", key='todo')
 addb = sg.Button("Add")
+listbox = sg.Listbox(values=functions.gtodos(), key='todos', 
+                     enable_events=True, size=[45, 10])
+editb = sg.Button("Edit")
 
 window = sg.Window('My To-Do App', 
-                   layout=[[label], [input_box, addb]], 
+                   layout=[[label], [input_box, addb], [listbox, editb]], 
                    font=('Helvetica', 13))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
+
     match event:
         case 'Add':
             todos = functions.gtodos()
-            ntodo = values['to-do'] + '\n'
+            ntodo = values['todo'] + '\n'
             todos.append(ntodo)
             functions.wtodos(todos)
+            window['todos'].update(values=todos)
+
+        case 'Edit':
+            todoe = values['todos'][0]
+            ntodo = values['todo']
+
+            todos = functions.gtodos()
+            index = todos.index(todoe)
+            todos[index] = ntodo + '\n'
+            functions.wtodos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
 
