2960e5559806c685b1b5e395671ab9ff2f8e095d
Tazir <tazirxxx@gmail.com>
2025-07-11 17:29:25 +0300

    Add complete feature/finish app

--- web.py
+++ web.py
@@ -17,14 +17,14 @@ st.subheader("This is a todo app")
 st.write("This app wil increase ur productivity")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    cbox = st.checkbox(todo, key=todo)
+    if cbox:
+        todos.pop(index)
+        functions.wtodos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
 st.text_input(label='Add a todo:', placeholder="Add a new todo...", 
               on_change=atodo, key='ntodo')
-
-
-
-print('Hello')
-st.session_state
